7500aa4fc6a4e1c56836eb03541be55cd56dcecf
Martin Donlon <git-martin@donlons.com>
2026-01-12 19:38:15 -0800

    Fixup merge issues

--- hdl/mycore.sv
+++ hdl/mycore.sv
@@ -15,7 +15,6 @@
 //  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
 //============================================================================
-<<<<<<< Updated upstream
 
 module emu
 (
@@ -93,19 +92,7 @@ module emu
 	// hint: supply 2'b00 to let the system control the LED.
 	output  [1:0] LED_POWER,
 	output  [1:0] LED_DISK,
-=======
 
-module emu
-(
-    //Master input clock
-    input         CLK_50M,
->>>>>>> Stashed changes
-
-    //Async reset from top-level module.
-    //Can be used as initial reset.
-    input         RESET,
-
-<<<<<<< Updated upstream
 	input         CLK_AUDIO, // 24.576 MHz
 	output [15:0] AUDIO_L,
 	output [15:0] AUDIO_R,
@@ -171,153 +158,6 @@ module emu
 	output  [6:0] USER_OUT,
 
 	input         OSD_STATUS
-=======
-    //Must be passed to hps_io module
-    inout  [48:0] HPS_BUS,
-
-    //Base video clock. Usually equals to CLK_SYS.
-    output        CLK_VIDEO,
-
-    //Multiple resolutions are supported using different CE_PIXEL rates.
-    //Must be based on CLK_VIDEO
-    output        CE_PIXEL,
-
-    //Video aspect ratio for HDMI. Most retro systems have ratio 4:3.
-    //if VIDEO_ARX[12] or VIDEO_ARY[12] is set then [11:0] contains scaled size instead of aspect ratio.
-    output [12:0] VIDEO_ARX,
-    output [12:0] VIDEO_ARY,
-
-    output  [7:0] VGA_R,
-    output  [7:0] VGA_G,
-    output  [7:0] VGA_B,
-    output        VGA_HS,
-    output        VGA_VS,
-    output        VGA_DE,    // = ~(VBlank | HBlank)
-    output        VGA_F1,
-    output [1:0]  VGA_SL,
-    output        VGA_SCALER, // Force VGA scaler
-    output        VGA_DISABLE, // analog out is off
-
-    input  [11:0] HDMI_WIDTH,
-    input  [11:0] HDMI_HEIGHT,
-    output        HDMI_FREEZE,
-    output        HDMI_BLACKOUT,
-
-`ifdef MISTER_FB
-    // Use framebuffer in DDRAM
-    // FB_FORMAT:
-    //    [2:0] : 011=8bpp(palette) 100=16bpp 101=24bpp 110=32bpp
-    //    [3]   : 0=16bits 565 1=16bits 1555
-    //    [4]   : 0=RGB  1=BGR (for 16/24/32 modes)
-    //
-    // FB_STRIDE either 0 (rounded to 256 bytes) or multiple of pixel size (in bytes)
-    output        FB_EN,
-    output  [4:0] FB_FORMAT,
-    output [11:0] FB_WIDTH,
-    output [11:0] FB_HEIGHT,
-    output [31:0] FB_BASE,
-    output [13:0] FB_STRIDE,
-    input         FB_VBL,
-    input         FB_LL,
-    output        FB_FORCE_BLANK,
-
-`ifdef MISTER_FB_PALETTE
-    // Palette control for 8bit modes.
-    // Ignored for other video modes.
-    output        FB_PAL_CLK,
-    output  [7:0] FB_PAL_ADDR,
-    output [23:0] FB_PAL_DOUT,
-    input  [23:0] FB_PAL_DIN,
-    output        FB_PAL_WR,
-`endif
-`endif
-
-    output        LED_USER,  // 1 - ON, 0 - OFF.
-
-    // b[1]: 0 - LED status is system status OR'd with b[0]
-    //       1 - LED status is controled solely by b[0]
-    // hint: supply 2'b00 to let the system control the LED.
-    output  [1:0] LED_POWER,
-    output  [1:0] LED_DISK,
-
-    // I/O board button press simulation (active high)
-    // b[1]: user button
-    // b[0]: osd button
-    output  [1:0] BUTTONS,
-
-    input         CLK_AUDIO, // 24.576 MHz
-    output [15:0] AUDIO_L,
-    output [15:0] AUDIO_R,
-    output        AUDIO_S,   // 1 - signed audio samples, 0 - unsigned
-    output  [1:0] AUDIO_MIX, // 0 - no mix, 1 - 25%, 2 - 50%, 3 - 100% (mono)
-
-    //ADC
-    inout   [3:0] ADC_BUS,
-
-    //SD-SPI
-    output        SD_SCK,
-    output        SD_MOSI,
-    input         SD_MISO,
-    output        SD_CS,
-    input         SD_CD,
-
-    //High latency DDR3 RAM interface
-    //Use for non-critical time purposes
-    output        DDRAM_CLK,
-    input         DDRAM_BUSY,
-    output  [7:0] DDRAM_BURSTCNT,
-    output [28:0] DDRAM_ADDR,
-    input  [63:0] DDRAM_DOUT,
-    input         DDRAM_DOUT_READY,
-    output        DDRAM_RD,
-    output [63:0] DDRAM_DIN,
-    output  [7:0] DDRAM_BE,
-    output        DDRAM_WE,
-
-    //SDRAM interface with lower latency
-    output        SDRAM_CLK,
-    output        SDRAM_CKE,
-    output [12:0] SDRAM_A,
-    output  [1:0] SDRAM_BA,
-    inout  [15:0] SDRAM_DQ,
-    output        SDRAM_DQML,
-    output        SDRAM_DQMH,
-    output        SDRAM_nCS,
-    output        SDRAM_nCAS,
-    output        SDRAM_nRAS,
-    output        SDRAM_nWE,
-
-`ifdef MISTER_DUAL_SDRAM
-    //Secondary SDRAM
-    //Set all output SDRAM_* signals to Z ASAP if SDRAM2_EN is 0
-    input         SDRAM2_EN,
-    output        SDRAM2_CLK,
-    output [12:0] SDRAM2_A,
-    output  [1:0] SDRAM2_BA,
-    inout  [15:0] SDRAM2_DQ,
-    output        SDRAM2_nCS,
-    output        SDRAM2_nCAS,
-    output        SDRAM2_nRAS,
-    output        SDRAM2_nWE,
-`endif
-
-    input         UART_CTS,
-    output        UART_RTS,
-    input         UART_RXD,
-    output        UART_TXD,
-    output        UART_DTR,
-    input         UART_DSR,
-
-    // Open-drain User port.
-    // 0 - D+/RX
-    // 1 - D-/TX
-    // 2..6 - USR2..USR6
-    // Set USER_OUT to 1 to read from USER_IN.
-    input   [6:0] USER_IN,
-    output  [6:0] USER_OUT,
-
-    input         OSD_STATUS
->>>>>>> Stashed changes
 );
 
 ///////// Default values for ports not used in this core /////////
@@ -326,10 +166,6 @@ assign ADC_BUS  = 'Z;
 assign USER_OUT = '1;
 assign {UART_RTS, UART_TXD, UART_DTR} = 0;
 assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
-<<<<<<< Updated upstream
-=======
-assign {SDRAM_DQ, SDRAM_A, SDRAM_BA, SDRAM_CLK, SDRAM_CKE, SDRAM_DQML, SDRAM_DQMH, SDRAM_nWE, SDRAM_nCAS, SDRAM_nRAS, SDRAM_nCS} = 'Z;
->>>>>>> Stashed changes
 assign {DDRAM_CLK, DDRAM_BURSTCNT, DDRAM_ADDR, DDRAM_DIN, DDRAM_BE, DDRAM_RD, DDRAM_WE} = '0;  
 
 assign VGA_SL = 0;
@@ -348,7 +184,6 @@ assign LED_DISK = 0;
 assign LED_POWER = 0;
 assign BUTTONS = 0;
 
-<<<<<<< Updated upstream
 assign NEC_POLLn = 0;
 assign NEC_READY = 1;
 assign NEC_INT = 0;
@@ -359,8 +194,6 @@ assign NEC_RESET = reset;
 assign NEC_ENABLEn = 0;
 
 
-=======
->>>>>>> Stashed changes
 //////////////////////////////////////////////////////////////////
 
 wire [1:0] ar = status[122:121];
@@ -370,7 +203,6 @@ assign VIDEO_ARY = (!ar) ? 12'd3 : 12'd0;
 
 `include "build_id.v" 
 localparam CONF_STR = {
-<<<<<<< Updated upstream
 	"MyCore;;",
 	"-;",
 	"O[122:121],Aspect ratio,Original,Full Screen,[ARC1],[ARC2];",
@@ -399,36 +231,6 @@ localparam CONF_STR = {
 	        // If CONF_STR options are changed in incompatible way, then change version number too,
 			  // so all options will get default values on first start.
 	"V,v",`BUILD_DATE 
-=======
-    "MyCore;;",
-    "-;",
-    "O[122:121],Aspect ratio,Original,Full Screen,[ARC1],[ARC2];",
-    "O[2],TV Mode,NTSC,PAL;",
-    "O[4:3],Noise,White,Red,Green,Blue;",
-    "-;",
-    "P1,Test Page 1;",
-    "P1-;",
-    "P1-, -= Options in page 1 =-;",
-    "P1-;",
-    "P1O[5],Option 1-1,Off,On;",
-    "d0P1F1,BIN;",
-    "H0P1O[10],Option 1-2,Off,On;",
-    "-;",
-    "P2,Test Page 2;",
-    "P2-;",
-    "P2-, -= Options in page 2 =-;",
-    "P2-;",
-    "P2S0,DSK;",
-    "P2O[7:6],Option 2,1,2,3,4;",
-    "-;",
-    "-;",
-    "T[0],Reset;",
-    "R[0],Reset and close OSD;",
-    "v,0;", // [optional] config version 0-99. 
-            // If CONF_STR options are changed in incompatible way, then change version number too,
-              // so all options will get default values on first start.
-    "V,v",`BUILD_DATE 
->>>>>>> Stashed changes
 };
 
 wire forced_scandoubler;
@@ -438,7 +240,6 @@ wire  [10:0] ps2_key;
 
 hps_io #(.CONF_STR(CONF_STR)) hps_io
 (
-<<<<<<< Updated upstream
 	.clk_sys(clk_sys),
 	.HPS_BUS(HPS_BUS),
 	.EXT_BUS(),
@@ -451,20 +252,6 @@ hps_io #(.CONF_STR(CONF_STR)) hps_io
 	.status_menumask({status[5]}),
 	
 	.ps2_key(ps2_key)
-=======
-    .clk_sys(clk_sys),
-    .HPS_BUS(HPS_BUS),
-    .EXT_BUS(),
-    .gamma_bus(),
-
-    .forced_scandoubler(forced_scandoubler),
-
-    .buttons(buttons),
-    .status(status),
-    .status_menumask({status[5]}),
-    
-    .ps2_key(ps2_key)
->>>>>>> Stashed changes
 );
 
 ///////////////////////   CLOCKS   ///////////////////////////////
@@ -472,15 +259,9 @@ hps_io #(.CONF_STR(CONF_STR)) hps_io
 wire clk_sys;
 pll pll
 (
-<<<<<<< Updated upstream
 	.refclk(CLK_50M),
 	.rst(0),
 	.outclk_0(clk_sys)
-=======
-    .refclk(CLK_50M),
-    .rst(0),
-    .outclk_0(clk_sys)
->>>>>>> Stashed changes
 );
 
 wire reset = RESET | status[0] | buttons[1];
@@ -496,7 +277,6 @@ wire [7:0] video;
 
 mycore mycore
 (
-<<<<<<< Updated upstream
 	.clk(clk_sys),
 	.reset(reset),
 	
@@ -511,22 +291,6 @@ mycore mycore
 	.VSync(VSync),
 
 	.video(video)
-=======
-    .clk(clk_sys),
-    .reset(reset),
-    
-    .pal(status[2]),
-    .scandouble(forced_scandoubler),
-
-    .ce_pix(ce_pix),
-
-    .HBlank(HBlank),
-    .HSync(HSync),
-    .VBlank(VBlank),
-    .VSync(VSync),
-
-    .video(video)
->>>>>>> Stashed changes
 );
 
 assign CLK_VIDEO = clk_sys;
@@ -543,7 +307,6 @@ reg  [26:0] act_cnt;
 always @(posedge clk_sys) act_cnt <= act_cnt + 1'd1; 
 assign LED_USER    = act_cnt[26]  ? act_cnt[25:18]  > act_cnt[7:0]  : act_cnt[25:18]  <= act_cnt[7:0];
 
-<<<<<<< Updated upstream
 reg [1:0] clk_div4;
 always_ff @(posedge clk_sys) begin
     clk_div4 <= clk_div4 + 2'd1;
@@ -551,6 +314,4 @@ end
 
 assign NEC_CLK = clk_div4[1];
 
-=======
->>>>>>> Stashed changes
 endmodule
